01ce791d52c9a5d443d0eb9fcd3061b78acdecc0
Pau Santana <127206784+psantana5@users.noreply.github.com>
2023-06-18 13:22:40 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,53 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get form data and sanitize inputs
-    $name = isset($_POST['name']) ? sanitizeInput($_POST['name']) : '';
-    $email = isset($_POST['email']) ? sanitizeInput($_POST['email']) : '';
-    $subject = isset($_POST['subject']) ? sanitizeInput($_POST['subject']) : '';
-    $message = isset($_POST['message']) ? sanitizeInput($_POST['message']) : '';
+    $name = sanitizeInput($_POST['name']);
+    $email = sanitizeInput($_POST['email']);
+    $subject = sanitizeInput($_POST['subject']);
+    $message = sanitizeInput($_POST['message']);
+
+    // Validate inputs
+    $requiredFields = array('name', 'email', 'subject', 'message');
+    foreach ($requiredFields as $field) {
+        if (empty($_POST[$field])) {
+            echo 'Please fill in all the required fields.';
+            exit;
+        }
+    }
 
     // Set recipient email and prepare email headers
-    $to = 'pausantanapi2@gmail.com';
-    $emailSubject = "Contact Form Submission: $subject";
+    $to = 'pausantanapi2@outlook.es';
+    $subject = "Contact Form Submission: $subject";
     $headers = "From: $name <$email>\r\n";
     $headers .= "Reply-To: $name <$email>\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     // Prepare email body
-    $emailBody = "Name: $name\r\n";
-    $emailBody .= "Email: $email\r\n";
-    $emailBody .= "Subject: $subject\r\n";
-    $emailBody .= "Message:\r\n$message";
+    $body = "Name: $name\r\n";
+    $body .= "Email: $email\r\n";
+    $body .= "Subject: $subject\r\n";
+    $body .= "Message:\r\n$message";
 
     // Send email
-    if (mail($to, $emailSubject, $emailBody, $headers)) {
-        echo 'Thank you for contacting us!';
+    if (mail($to, $subject, $body, $headers)) {
+        echo 'Thank you for contacting me!';
     } else {
         echo 'Oops! An error occurred.';
     }
 }
+
+/**
+ * Sanitize user input to prevent email injection and other security issues
+ *
+ * @param string $input The user input string to sanitize
+ * @return string The sanitized input
+ */
+function sanitizeInput($input)
+{
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    return $input;
+}
+?>
